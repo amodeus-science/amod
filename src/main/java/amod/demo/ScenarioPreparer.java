367930b1c93647e5d36a27c11eddc7b53b914f1a
clruch <claudio.ruch@gmail.com>
2018-03-13 13:20:30 +0100

    added glpk test, removed facilities...

--- src/main/java/amod/demo/ScenarioPreparer.java
+++ src/main/java/amod/demo/ScenarioPreparer.java
@@ -9,12 +9,10 @@ import org.matsim.api.core.v01.population.Population;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.scenario.ScenarioUtils;
-import org.matsim.facilities.ActivityFacilities;
 
 import amod.demo.ext.Static;
 import ch.ethz.idsc.amodeus.options.ScenarioOptions;
 import ch.ethz.idsc.amodeus.prep.ConfigCreator;
-import ch.ethz.idsc.amodeus.prep.FacilitiesPreparer;
 import ch.ethz.idsc.amodeus.prep.NetworkPreparer;
 import ch.ethz.idsc.amodeus.prep.PopulationPreparer;
 import ch.ethz.idsc.amodeus.prep.VirtualNetworkPreparer;
@@ -54,10 +52,6 @@ public enum ScenarioPreparer {
         /** adaption of MATSim population, e.g., radius cutting */
         Population population = scenario.getPopulation();
         PopulationPreparer.run(network, population, scenarioOptions, config);
-
-        /** adaption of MATSim facilities, e.g., radius cutting */
-        ActivityFacilities facilities = scenario.getActivityFacilities();
-        FacilitiesPreparer.run(facilities, population, scenarioOptions);
 
         /** creating a virtual network, e.g., for dispatchers using a graph structure on the city */
         VirtualNetworkPreparer.run(network, population, scenarioOptions);
