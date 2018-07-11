4880f1d9512e94fa1713f631564b258cb7f0ff87
Jan Hakenberg <jan.hakenberg@gmail.com>
2018-07-11 02:41:31 +0200

    remove aido analysis element

--- src/main/java/amod/aido/AidoHtmlReport.java
+++ src/main/java/amod/aido/AidoHtmlReport.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import ch.ethz.idsc.amodeus.analysis.AnalysisSummary;
+import ch.ethz.idsc.amodeus.analysis.element.DistanceElement;
 import ch.ethz.idsc.amodeus.analysis.report.HtmlBodyElement;
 import ch.ethz.idsc.amodeus.analysis.report.HtmlReportElement;
 import ch.ethz.idsc.tensor.RealScalar;
@@ -14,45 +15,47 @@ import ch.ethz.idsc.tensor.Tensors;
 
 /* package */ class AidoHtmlReport implements HtmlReportElement {
 
-    @SuppressWarnings("unused")
-    private final AidoAnalysisElement aidoAnalysisElement;
     private Scalar totalMeanWaitingTime;
     private Scalar totalEfficiencyRatio; /* empty distance divided by total distance */
     private Scalar numberOfVehicles;
-
-    // TODO figure out if input param is needed, otherwise remove
-    /* package */ public AidoHtmlReport(AidoAnalysisElement aidoAnalysisElement) {
-        this.aidoAnalysisElement = aidoAnalysisElement;
-    }
 
     @Override
     public Map<String, HtmlBodyElement> process(AnalysisSummary analysisSummary) {
 
         this.totalMeanWaitingTime = analysisSummary.getWaitingTimes().totalWaitTimeMean;
-        this.totalEfficiencyRatio = RealScalar.of((analysisSummary.getDistanceElement().totalDistancePicku + //
-                analysisSummary.getDistanceElement().totalDistanceRebal) / analysisSummary.getDistanceElement().totalDistance);
+
+        {
+            DistanceElement distanceElement = analysisSummary.getDistanceElement();
+            double sum = distanceElement.totalDistancePicku + distanceElement.totalDistanceRebal;
+            // if totalDistance == 0.0, the ratio is NaN
+            this.totalEfficiencyRatio = RealScalar.of(sum / distanceElement.totalDistance);
+        }
+
         this.numberOfVehicles = RealScalar.of(analysisSummary.getSimulationInformationElement().vehicleSize());
 
-        HtmlBodyElement aRElement = new HtmlBodyElement();
-        aRElement.getHTMLGenerator().insertTextLeft(aRElement.getHTMLGenerator().bold("Individual Scores") + //
-                "\n\t" + "mean waiting time:" + //
-                "\n\t" + "empty distance / total distance:" + //
-                "\n\t" + "number of RoboTaxis:" //
-        );
-        aRElement.getHTMLGenerator().insertTextLeft(" " + //
-                "\n" + totalMeanWaitingTime + //
-                "\n" + totalEfficiencyRatio + //
-                "\n" + numberOfVehicles //
-        );
-        aRElement.getHTMLGenerator().newLine();
-
         Map<String, HtmlBodyElement> bodyElements = new HashMap<>();
-        bodyElements.put("Aido Scores", aRElement);
+        {
+            HtmlBodyElement aRElement = new HtmlBodyElement();
+            aRElement.getHTMLGenerator().insertTextLeft(aRElement.getHTMLGenerator().bold("Individual Scores") + //
+                    "\n\t" + "mean waiting time:" + //
+                    "\n\t" + "empty distance / total distance:" + //
+                    "\n\t" + "number of RoboTaxis:" //
+            );
+            aRElement.getHTMLGenerator().insertTextLeft(" " + //
+                    "\n" + totalMeanWaitingTime + //
+                    "\n" + totalEfficiencyRatio + //
+                    "\n" + numberOfVehicles //
+            );
+            aRElement.getHTMLGenerator().newLine();
+
+            bodyElements.put("Aido Scores", aRElement);
+        }
         return bodyElements;
 
     }
 
-    /* package */ Tensor getFinalScore() {
+    /** @return */
+    public Tensor getFinalScore() {
         return Tensors.of(totalMeanWaitingTime, totalEfficiencyRatio, numberOfVehicles);
     }
 
