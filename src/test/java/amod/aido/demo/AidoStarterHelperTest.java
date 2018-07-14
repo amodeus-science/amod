f3f4976d2bc205d0b939dc70b4f605c827aab320
Jan Hakenberg <jan.hakenberg@gmail.com>
2018-07-14 14:37:32 +0200

    suggestion by @sebhoerl

--- src/test/java/amod/aido/demo/AidoStarterHelperTest.java
+++ src/test/java/amod/aido/demo/AidoStarterHelperTest.java
@@ -3,7 +3,6 @@ package amod.aido.demo;
 
 import java.io.IOException;
 import java.net.UnknownHostException;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -33,21 +32,22 @@ public class AidoStarterHelperTest extends TestCase {
     public void testSanFrancisco() throws UnknownHostException, IOException, Exception {
 
         List<Integer> list = IntStream.range(0, 3).boxed().collect(Collectors.toList());
-        Collections.shuffle(list);
-
-        // TODO if i put 2 instead of 1 -> doesn't finish but exception in traffic link data
+        // Previously:
+        // if i put 2 instead of 1 -> doesn't finish but exception in traffic link data
         // ... cannot run more than 1 scenario at runtime
-        list = list.stream().limit(1).collect(Collectors.toList());
+        // TODO right now SanFrancisco has to run last -> don't shuffle
+        // Collections.shuffle(list);
+        list = list.stream().limit(3).collect(Collectors.toList());
         for (int index : list)
             switch (index) {
             case 0:
-                guest().run("SanFrancisco", 0.01, 5);
-                break;
-            case 1:
                 guest().run("TelAviv", 0.001, 6);
                 break;
-            case 2:
+            case 1:
                 guest().run("Santiago", 0.001, 10);
+                break;
+            case 2:
+                guest().run("SanFrancisco", 0.01, 5);
                 break;
             default:
                 throw new RuntimeException("out of range");
