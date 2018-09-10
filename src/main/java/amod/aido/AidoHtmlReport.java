6e1498d2a9dddce258f168193c35a81accbd3d5a
Dejan Milojevic <dejanmi@student.ethz.ch>
2018-09-10 14:08:57 -0700

    merge conflict

--- src/main/java/amod/aido/AidoHtmlReport.java
+++ src/main/java/amod/aido/AidoHtmlReport.java
@@ -4,6 +4,7 @@ package amod.aido;
 import java.util.HashMap;
 import java.util.Map;
 
+import ch.ethz.idsc.amodeus.aido.AidoScoreElement;
 import ch.ethz.idsc.amodeus.analysis.AnalysisSummary;
 import ch.ethz.idsc.amodeus.analysis.element.DistanceElement;
 import ch.ethz.idsc.amodeus.analysis.report.HtmlBodyElement;
@@ -12,12 +13,22 @@ import ch.ethz.idsc.tensor.RealScalar;
 import ch.ethz.idsc.tensor.Scalar;
 import ch.ethz.idsc.tensor.Tensor;
 import ch.ethz.idsc.tensor.Tensors;
+import ch.ethz.idsc.tensor.alg.Transpose;
+import ch.ethz.idsc.tensor.red.Total;
 
 /* package */ class AidoHtmlReport implements HtmlReportElement {
 
+    /** relative to report folder */
+    private static final String IMAGE_FOLDER = "../data";
+    // ---
+    private final AidoScoreElement aidoScoreElement;
     private Scalar totalMeanWaitingTime;
     private Scalar totalEfficiencyRatio; /* empty distance divided by total distance */
     private Scalar numberOfVehicles;
+
+    public AidoHtmlReport(AidoScoreElement aidoScoreElement) {
+        this.aidoScoreElement = aidoScoreElement;
+    }
 
     @Override
     public Map<String, HtmlBodyElement> process(AnalysisSummary analysisSummary) {
@@ -36,15 +47,22 @@ import ch.ethz.idsc.tensor.Tensors;
         Map<String, HtmlBodyElement> bodyElements = new HashMap<>();
         {
             HtmlBodyElement aRElement = new HtmlBodyElement();
-            aRElement.getHTMLGenerator().insertTextLeft(aRElement.getHTMLGenerator().bold("Individual Scores") + //
-                    "\n\t" + "mean waiting time:" + //
-                    "\n\t" + "empty distance / total distance:" + //
-                    "\n\t" + "number of RoboTaxis:" //
+
+            aRElement.getHTMLGenerator().insertTextLeft(aRElement.getHTMLGenerator().bold("Scores during Simulation"));
+            aRElement.getHTMLGenerator().newLine();
+            aRElement.getHTMLGenerator().insertImg(IMAGE_FOLDER + "/" + AidoExport.FILENAME_SCORE_INCR + ".png", 800, 600);
+            aRElement.getHTMLGenerator().insertImg(IMAGE_FOLDER + "/" + AidoExport.FILENAME_SCORE_INTG + ".png", 800, 600);
+            aRElement.getHTMLGenerator().insertImg(IMAGE_FOLDER + "/" + AidoExport.FILENAME_SCORE3_INTG + ".png", 800, 600);
+
+            aRElement.getHTMLGenerator().insertTextLeft(aRElement.getHTMLGenerator().bold("Final Scores") + //
+                    "\n\t" + "final service quality score:" + //
+                    "\n\t" + "final efficiency score:" + //
+                    "\n\t" + "final fleet size score:" //
             );
             aRElement.getHTMLGenerator().insertTextLeft(" " + //
-                    "\n" + totalMeanWaitingTime + //
-                    "\n" + totalEfficiencyRatio + //
-                    "\n" + numberOfVehicles //
+                    "\n" + Total.of(Transpose.of(aidoScoreElement.getScoreDiffHistory()).get(1)) + //
+                    "\n" + Total.of(Transpose.of(aidoScoreElement.getScoreDiffHistory()).get(2)) + //
+                    "\n" + Total.of(Transpose.of(aidoScoreElement.getScoreDiffHistory()).get(3)) //
             );
             aRElement.getHTMLGenerator().newLine();
 
