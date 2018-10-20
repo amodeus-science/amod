60b02edf215fe4e8f4e19551b708651383839756
Jan Hakenberg <jan.hakenberg@gmail.com>
2018-10-20 17:51:52 +0200

    override settings with environment variables

--- src/main/java/amod/aido/AidoHost.java
+++ src/main/java/amod/aido/AidoHost.java
@@ -2,6 +2,7 @@
 package amod.aido;
 
 import java.io.File;
+import java.util.Objects;
 
 import amod.aido.core.AidoDispatcherHost;
 import amod.aido.core.AidoScoreElement;
@@ -30,6 +31,9 @@ import ch.ethz.idsc.tensor.sca.Round;
 public enum AidoHost {
     ;
     public static final int PORT = 9382;
+    private static final String ENV_SCENARIO = "SCENARIO";
+    private static final String ENV_FLEET_SIZE = "FLEET_SIZE";
+    private static final String ENV_REQUESTS_SIZE = "REQUESTS_SIZE";
 
     public static void main(String[] args) throws Exception {
         /** open String server and wait for initial command */
@@ -42,6 +46,11 @@ public enum AidoHost {
             System.out.println("AidoHost config: " + config);
             Thread.sleep(1000);
             String scenarioTag = config.Get(0).toString();
+            {
+                String env = System.getenv(ENV_SCENARIO);
+                if (Objects.nonNull(env))
+                    env = scenarioTag;
+            }
 
             /** download the chosen scenario */
             try {
@@ -78,6 +87,25 @@ public enum AidoHost {
             Tensor config2 = Tensors.fromString(readLine2);
             int numReqDes = config2.Get(0).number().intValue();
             int fleetSize = config2.Get(1).number().intValue();
+
+            {
+                String env = System.getenv(ENV_REQUESTS_SIZE);
+                if (Objects.nonNull(env))
+                    try {
+                        numReqDes = Integer.parseInt(env);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+            }
+            {
+                String env = System.getenv(ENV_FLEET_SIZE);
+                if (Objects.nonNull(env))
+                    try {
+                        fleetSize = Integer.parseInt(env);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+            }
 
             /** run second part of preparer */
             preparer.run2(numReqDes);
