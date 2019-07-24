12a690f6bb29e37e73676c3d486b5223ff377560
Claudio <claudio.ruch@gmail.com>
2019-07-24 13:18:41 +0200

    minor.

--- src/main/java/amod/aido/core/CleanAidoScenarios.java
+++ src/main/java/amod/aido/core/CleanAidoScenarios.java
@@ -10,8 +10,8 @@ public enum CleanAidoScenarios {
     public static void now() {
         String[] files = new String[] { "AmodeusOptions.properties", "av.xml", //
                 "matsimConfig.xml", "personAtrributes-with-subpopulation.xml", //
-                "preparedNetwork.xml", "config_full.xml", "linkSpeedData", //
-                "config.xml", "preparedConfig.xml", "preparedPopulation.xml", //
+                "preparedNetwork.xml.gz","preparedNetwork.xml", "config_full.xml", "linkSpeedData", //
+                "config.xml", "preparedConfig.xml", "preparedPopulation.xml","preparedPopulation.xml.gz", //
                 "population.xml" };
         for (String file : files) {
             boolean ok = new File(file).delete();
