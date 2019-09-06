3d4294ca483abf9d6dfbc9920cc045c349c410cc
clruch <claudio.ruch@gmail.com>
2019-09-06 15:47:42 +0200

    first version of TripMaxSpeedFilter.

--- src/main/java/amod/scenario/tripmodif/ChicagoOnlineTripBasedModifier.java
+++ src/main/java/amod/scenario/tripmodif/ChicagoOnlineTripBasedModifier.java
@@ -5,6 +5,7 @@ import java.util.Random;
 
 import org.matsim.api.core.v01.network.Network;
 
+import amod.scenario.tripfilter.TripMaxSpeedFilter;
 import ch.ethz.idsc.amodeus.net.FastLinkLookup;
 
 public class ChicagoOnlineTripBasedModifier extends TripBasedModifier {
@@ -12,8 +13,7 @@ public class ChicagoOnlineTripBasedModifier extends TripBasedModifier {
     public ChicagoOnlineTripBasedModifier(Random random, Network network, //
             FastLinkLookup fll, File vNetworkExportFile) {
         addModifier(new ChicagoTripStartTimeResampling(random));
-        addModifier(new OriginDestinationCentroidResampling(random, network, //
-                fll, vNetworkExportFile));
+        addModifier(new OriginDestinationCentroidResampling(random, network, fll, vNetworkExportFile));
     }
 
 }
