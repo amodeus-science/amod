3d67bcf81b7dbbb2aa08e251e5f50d10ff43c333
Sebastian Hörl <hoerl.sebastian@gmail.com>
2020-05-05 17:32:29 +0200

    Towards multimodal dvrp

--- src/main/java/ch/ethz/idsc/amod/router/CustomRouter.java
+++ src/main/java/ch/ethz/idsc/amod/router/CustomRouter.java
@@ -7,12 +7,12 @@ import java.util.concurrent.Future;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.network.Node;
 import org.matsim.api.core.v01.population.Person;
+import org.matsim.contrib.dvrp.run.ModalProviders.InstanceGetter;
 import org.matsim.core.router.util.LeastCostPathCalculator.Path;
 import org.matsim.vehicles.Vehicle;
 
 import ch.ethz.idsc.amodeus.dispatcher.core.RoboTaxi;
 import ch.ethz.idsc.amodeus.util.math.GlobalAssert;
-import ch.ethz.matsim.av.config.operator.RouterConfig;
 import ch.ethz.matsim.av.router.AVRouter;
 
 /** This is a nonfunctional sample demonstrating of how to include a custom router
@@ -41,7 +41,7 @@ import ch.ethz.matsim.av.router.AVRouter;
      * example the DefaultAVRouter */
     public static class Factory implements AVRouter.Factory {
         @Override
-        public AVRouter createRouter(RouterConfig routerConfig, Network network) {
+        public AVRouter createRouter(InstanceGetter inject) {
             return new CustomRouter();
         }
     }
