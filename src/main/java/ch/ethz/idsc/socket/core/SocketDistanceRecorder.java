71f417b2d0a31d3ec00f60f8a436dee0e95a7914
JoelGaechter <joelgaechter@bluewin.ch>
2020-05-28 11:03:20 +0200

    adapt to amodeus changes

--- src/main/java/ch/ethz/idsc/socket/core/SocketDistanceRecorder.java
+++ src/main/java/ch/ethz/idsc/socket/core/SocketDistanceRecorder.java
@@ -1,26 +1,25 @@
 /* amodeus - Copyright (c) 2018, ETH Zurich, Institute for Dynamic Systems and Control */
 package ch.ethz.idsc.socket.core;
 
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.HashMap;
+import java.util.Map;
 
 import ch.ethz.idsc.amodeus.net.MatsimAmodeusDatabase;
 import ch.ethz.idsc.amodeus.net.SimulationObject;
 import ch.ethz.idsc.tensor.Tensor;
 
 /* package */ class SocketDistanceRecorder {
-    private final List<SocketVehicleStatistic> list;
+    private final MatsimAmodeusDatabase db;
+    private final Map<Integer, SocketVehicleStatistic> map = new HashMap<>();
 
-    public SocketDistanceRecorder(int numVehicles, MatsimAmodeusDatabase db) {
-        list = IntStream.range(0, numVehicles) //
-                .mapToObj(i -> new SocketVehicleStatistic(db)) //
-                .collect(Collectors.toList());
+    public SocketDistanceRecorder(MatsimAmodeusDatabase db) {
+        this.db = db;
     }
 
     Tensor distance(SimulationObject simulationObject) {
         return simulationObject.vehicles.stream() //
-                .map(vehicleContainer -> list.get(vehicleContainer.vehicleIndex).distance(vehicleContainer)) //
+                .map(vehicleContainer -> map.computeIfAbsent(vehicleContainer.vehicleIndex, //
+                        i -> new SocketVehicleStatistic(db)).distance(vehicleContainer)) //
                 .reduce(Tensor::add) //
                 .orElse(StaticHelper.ZEROS.copy());
     }
