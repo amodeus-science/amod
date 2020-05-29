c6bde05413c1270ce2bd894c984d8b15f6efe928
JoelGaechter <joelgaechter@bluewin.ch>
2020-05-29 13:29:22 +0200

    bug fix

--- src/test/java/ch/ethz/idsc/amod/DemoTest.java
+++ src/test/java/ch/ethz/idsc/amod/DemoTest.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 
 import ch.ethz.idsc.amodtaxi.scenario.ScenarioCreation;
+import ch.ethz.idsc.amodtaxi.scenario.sanfrancisco.TraceFileChoice;
 import org.junit.AfterClass;
 import org.junit.Test;
 
@@ -34,7 +35,10 @@ public class DemoTest {
         new File(workingDirectory, "network.xml.gz").delete();
         new File(workingDirectory, "network_pt2matsim.xml").delete();
         new File(workingDirectory, "pt2matsim_settings.xml").delete();
-        DeleteDirectory.of(new File(workingDirectory, "default_cabspottingdata"), 1, 5); // was not writable locally
+
+        if (TraceFileChoice.DEFAULT_DATA.exists())
+            DeleteDirectory.of(TraceFileChoice.DEFAULT_DATA, 1, 5);
+
         DeleteDirectory.of(new File(workingDirectory, "Scenario"), 2, 14);
         DeleteDirectory.of(new File(workingDirectory, "2008-06-04"), 2, 14);
         DeleteDirectory.of(new File(workingDirectory, "output"), 5, 11000);
